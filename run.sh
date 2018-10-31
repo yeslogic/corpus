7d507a5b9c90671f97173a03110cb5a3ec3101f5
Michael Day <mikeday@yeslogic.com>
2018-10-31 17:43:10 +1100

    Updated instructions and run script.

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-# takes about twenty minutes
+for s in bn gu hi kn ml or pa si ta te ; do
+    echo $s words
+    ./corpus $s < wikipedia/${s}wiki-20181001-pages-articles-multistream.xml > data/words.$s
+done
 
 for s in bn gu hi kn ml or pa si ta te ; do
-    echo $s
-    ./corpus $s < ${s}wiki-20181001-pages-articles-multistream.xml > out.$s
+    echo $s syllables
+    ./syllables $s < data/words.$s > data/syl.$s
 done
